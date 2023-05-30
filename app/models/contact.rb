cd994714caccdbbeab1a348ad2512b8ed01bee5f
Tsubasa Toda <t.tsubasa310@gmail.com>
2023-05-30 13:23:42 +0900

    working on it

---
+++ app/models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ApplicationRecord
+  # validates :first_name, presence: true, on: :create
+  # validates :last_name, presence: true
+  # validates :email, presence: true
+  # validates :message, presence: true
+end
